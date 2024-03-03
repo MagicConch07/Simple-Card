c0e9a915dac36f6624d4040fb5bf409ba31035c3
Mk <minrim0615@gmail.com>
2024-03-03 18:48:53 +0900

    Card Select

--- Lobster1/Assets/10_UI/Uxml/UI.cs
+++ Lobster1/Assets/10_UI/Uxml/UI.cs
@@ -9,7 +9,11 @@ public class UI : MonoBehaviour
     private UIDocument _uiDocument;
     private Camera _mainCam;
 
+    //private ValueTuple<VisualElement, bool>[] _cards = new ValueTuple<VisualElement, bool>[5];
     private VisualElement[] _cards = new VisualElement[5];
+
+    private int _isSelectStack;
+    private bool _isPick;
 
     private void Awake()
     {
@@ -26,18 +30,25 @@ public class UI : MonoBehaviour
         for (int i = 0; i < _cards.Length; ++i)
         {
             _cards[i].RegisterCallback<MouseEnterEvent>(OnMouseEnterCard);
+            _cards[i].RegisterCallback<MouseOutEvent>(OnMouseOutCard);
+            _cards[i].RegisterCallback<ClickEvent>(OnClickCard);
         }
         
-        for (int i = 0; i < _cards.Length; ++i)
-        {
-            _cards[i].RegisterCallback<MouseOutEvent>(OnMouseOutCard);
-        }
+    }
+
+    private void OnClickCard(ClickEvent evt)
+    {
+        var card = (VisualElement)evt.currentTarget;
+        card.AddToClassList("card-select");
+        
     }
 
     private void OnMouseEnterCard(MouseEnterEvent evt)
     {
         var card = (VisualElement)evt.currentTarget;
         card.AddToClassList("card-pick");
+
+        //var tuple = ((VisualElement)evt.currentTarget, false);
     }
     
     private void OnMouseOutCard(MouseOutEvent evt)
@@ -45,5 +56,7 @@ public class UI : MonoBehaviour
         var card = (VisualElement)evt.currentTarget;
         card.RemoveFromClassList("card-pick");
     }
+    
+    
     
 }
